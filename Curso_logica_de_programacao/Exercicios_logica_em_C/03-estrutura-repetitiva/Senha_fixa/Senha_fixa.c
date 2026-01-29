cbe4448f61c390a907d73787ff2442e682e773fb
Raniel Honorato <ranielhonorato@hotmail.com>
2026-01-29 00:01:50 -0300

    Estudos e exercícios lógica 29/01/26 (#2)

--- Curso_logica_de_programacao/Exercicios_logica_em_C/03-estrutura-repetitiva/Senha_fixa/Senha_fixa.c
+++ Curso_logica_de_programacao/Exercicios_logica_em_C/03-estrutura-repetitiva/Senha_fixa/Senha_fixa.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
-    
+    int senha;
+
+    // Usuario define a senha
+    printf("Defina a senha: ");
+    scanf("%d", &senha);
+
+    // Loop para validar se a senha digitada é a mesma definida pelo usuario
+    int valida_senha;
+    printf("Digite sua senha para ter acesso: ");
+    do {
+        scanf("%d", &valida_senha);
+
+        if (valida_senha != senha) {
+            printf("Senha invalida! Tente novamente: ");
+        }
+
+    } while (valida_senha != senha);
+
+    printf("\nAcesso permitido");
 
     return 0;
 }
