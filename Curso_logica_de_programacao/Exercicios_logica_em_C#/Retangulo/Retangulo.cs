8091b3efa28b8c2f64d6d8508d8a0cd77ab21898
Raniel Honorato <ranielhonorato@hotmail.com>
2026-02-05 19:27:29 -0300

    Fix: Alterações no .gitignore e novo exercício em C#

---
+++ Curso_logica_de_programacao/Exercicios_logica_em_C#/Retangulo/Retangulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Progama
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double base_retangulo, altura, area;
+
+            Console.Write("Base retangulo: ");
+            base_retangulo = double.Parse(Console.ReadLine(), CI);
+            Console.Write("Altura retangulo: ");
+            altura = double.Parse(Console.ReadLine(), CI);
+
+            area = base_retangulo * altura;
+            double perimetro = 2 * (base_retangulo+altura);
+            double diagonal =  Math.Sqrt(Math.Pow(base_retangulo, 2) + Math.Pow(altura, 2));
+
+            Console.WriteLine("Area = " + area.ToString("F4", CI));
+            Console.WriteLine("Perimetro = " + perimetro.ToString("F4", CI));
+            Console.WriteLine("Diagonal = " + diagonal.ToString("F4", CI));
+
+        }
+    }
+}
